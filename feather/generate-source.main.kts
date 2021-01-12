6ea278625b50ba631a84dc3af9c4541663b221d3
DevSrSouza <devsrsouza@gmail.com>
2021-01-12 16:29:56 -0300

    Update feather with the new version from svg-to-compose

--- feather/generate-source.main.kts
+++ feather/generate-source.main.kts
@@ -3,7 +3,7 @@
 @file:Repository("https://jetbrains.bintray.com/trove4j")
 
 // svg-to-compose
-@file:DependsOn("com.github.DevSrSouza:svg-to-compose:0.3.0")
+@file:DependsOn("com.github.DevSrSouza:svg-to-compose:0.4.0")
 @file:DependsOn("com.google.guava:guava:23.0")
 @file:DependsOn("com.android.tools:sdk-common:27.2.0-alpha16")
 @file:DependsOn("com.android.tools:common:27.2.0-alpha16")
@@ -51,7 +51,7 @@ println("Generating all svg to compose")
 
 Svg2Compose.parse(
     applicationIconPackage = "compose.icons",
-    accessorName = "feather_icons",
+    accessorName = "FeatherIcons",
     outputSourceDirectory = srcDir,
     vectorsDirectory = iconsDir,
     type = VectorType.SVG,
