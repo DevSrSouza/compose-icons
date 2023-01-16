a2e03461f02751768b3d6036acc56a715199efe7
Waqas Tahir <wakaztahir@gmail.com>
2023-01-16 20:02:43 +0500

    replace trov4j with mavenCentral and google repo url

--- materialdesignicons/generate-source.main.kts
+++ materialdesignicons/generate-source.main.kts
@@ -1,8 +1,7 @@
 @file:Repository("https://jitpack.io")
-@file:Repository("https://maven.google.com")
-@file:Repository("https://jetbrains.bintray.com/trove4j")
-@file:Repository("file:///home/devsrsouza/.m2/repository")
 @file:Repository("https://dl.google.com/dl/android/maven2/")
+@file:Repository("https://repo.maven.apache.org/maven2/")
+@file:Repository("file:///home/devsrsouza/.m2/repository")
 @file:Repository("https://repo.maven.apache.org/maven2/")
 
 // svg-to-compose
